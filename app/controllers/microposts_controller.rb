71d49a00c49a161a623157ff4cd699cbe08d1ba5
paramdhal <paramdhal@gmail.com>
2012-11-02 18:42:26 +0000

    added feed items to micropost controller

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -8,6 +8,7 @@ class MicropostsController < ApplicationController
             redirect_to root_url
         else
             render 'static_pages/home'
+            @feed_items = []
         end        
     end
 
