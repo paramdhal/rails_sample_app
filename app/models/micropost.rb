8a8d2127cdf95cf523f8581a0a552e7211f1daa4
paramdhal <paramdhal@gmail.com>
2012-11-02 12:45:38 +0000

    Added has_many and belongs_to methods

---
+++ app/models/micropost.rb
@@ -0,0 +1,7 @@
+class Micropost < ActiveRecord::Base
+  attr_accessible :content
+  belongs_to :user
+  default_scope order: 'microposts.created_at DESC'
+
+  validates :user_id, presence: true
+end
